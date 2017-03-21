9b49e112be1c03eb74e1ad362b179d0721fad37e
Mrunal Patel <mrunalp@gmail.com>
2017-03-21 16:23:00 -0700

    Add initial code for crictl
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

---
+++ cmd/crictl/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/urfave/cli"
+	"google.golang.org/grpc"
+)
+
+const (
+	defaultTimeout = 10 * time.Second
+)
+
+func getRuntimeClientConnection(context *cli.Context) (*grpc.ClientConn, error) {
+	runtimeEndpoint := context.GlobalString("runtime-endpoint")
+	if runtimeEndpoint == "" {
+		return nil, fmt.Errorf("--runtime-endpoint is not set")
+	}
+	conn, err := grpc.Dial(context.GlobalString("runtime-endpoint"), grpc.WithInsecure(), grpc.WithTimeout(context.GlobalDuration("timeout")),
+		grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+			return net.DialTimeout("unix", addr, timeout)
+		}))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect: %v", err)
+	}
+	return conn, nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "crictl"
+	app.Usage = "client for CRI"
+	app.Version = "0.0.1"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "runtime-endpoint",
+			Usage: "CRI server runtime endpoint",
+		},
+		cli.StringFlag{
+			Name:  "image-endpoint",
+			Usage: "CRI server image endpoint",
+		},
+		cli.DurationFlag{
+			Name:  "timeout",
+			Value: defaultTimeout,
+			Usage: "Timeout of connecting to server",
+		},
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "Enable debug output",
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		glog.Fatal(err)
+	}
+}
