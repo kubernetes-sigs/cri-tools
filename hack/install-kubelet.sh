a8a847a4e27207b82543e3cb9e4a12e50aa44407
Davanum Srinivas <davanum@gmail.com>
2020-05-24 17:08:05 -0400

    install cni plugins
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- hack/install-kubelet.sh
+++ hack/install-kubelet.sh
@@ -20,6 +20,16 @@ set -o pipefail
 
 set -x
 
+CNI_VERSION="v0.8.6"
+echo "Installing CNI binaries ..." \
+    && export ARCH=$(dpkg --print-architecture | sed 's/ppc64el/ppc64le/' | sed 's/armhf/arm/') \
+    && export CNI_TARBALL="${CNI_VERSION}/cni-plugins-linux-${ARCH}-${CNI_VERSION}.tgz" \
+    && export CNI_URL="https://github.com/containernetworking/plugins/releases/download/${CNI_TARBALL}" \
+    && curl -sSL --retry 5 --output /tmp/cni.tgz "${CNI_URL}" \
+    && mkdir -p /opt/cni/bin \
+    && tar -C /opt/cni/bin -xzf /tmp/cni.tgz \
+    && rm -rf /tmp/cni.tgz
+
 # Install kubelet
 git clone https://github.com/kubernetes/kubernetes $GOPATH/src/k8s.io/kubernetes
 cd $GOPATH/src/k8s.io/kubernetes
--- hack/run-critest.sh
+++ hack/run-critest.sh
@@ -30,7 +30,7 @@ else
 fi
 
 # Start dockershim first
-/usr/local/bin/kubelet --v=3 --logtostderr --network-plugin=kubenet --pod-cidr=10.180.0.0/24 &
+/usr/local/bin/kubelet --v=3 --logtostderr &
 
 # Wait a while for dockershim starting.
 sleep 30
