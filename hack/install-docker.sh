58fa354f6eeb188aaddeaa27a70dd27638c01c77
Martin Hickey <martin.hickey@ie.ibm.com>
2020-07-01 15:04:47 +0000

    Specify docker version to use
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- hack/install-docker.sh
+++ hack/install-docker.sh
@@ -43,8 +43,14 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-cache madison docker-ce
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Docker is downgraded because exec process in 18.x doesn't inherit additional group id from the init process.
+# See more details at https://github.com/moby/moby/issues/38865.
+if [ "$arch" == amd64 ]; then
+	sudo apt-get -y --allow-downgrades install docker-ce=5:18.09.5~3-0~ubuntu-bionic
+elif [ "$arch" == ppc64el ]; then
+	sudo apt-get -y --allow-downgrades install docker-ce=18.06.0~ce~3-0~ubuntu
+fi
 
 # Restart docker daemon.
 sudo service docker restart
